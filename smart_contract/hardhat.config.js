8f873d3b4f9f1e2bbe6073006e9f50fbe683caf7
heyitspratham <theprathamshukla@gmail.com>
2022-04-15 23:57:53 +0530

    first commit

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+ 
+module.exports = {
+  solidity: "0.8.4",
+  network:{
+    rinkeby:{
+      url: "https://eth-goerli.alchemyapi.io/v2/dsnHX0_eoxetGGnikf1qxrl5PFaZvyBC",
+      account: ['0e22d41f8e2874ab81a5923abe37b2069817aa4bc329633d15d24d289e7a81fd'],
+    }
+  }
+};
