4380c84dbce69ea91865531b98c5b47fc06df147
heyitspratham <theprathamshukla@gmail.com>
2022-04-17 18:34:01 +0530

    contract

--- smart_contract/contracts/MintProfileImage.sol
+++ smart_contract/contracts/MintProfileImage.sol
@@ -2,8 +2,23 @@
 
 pragma solidity ^0.8.2;
 
-import @openzeppelin/contracts/token/ERC721/ERC721.sol;
-import @openzeppelin/contracts/access/Ownable.sol;
-import @openzeppelin/contracts/utils/Counters.sol;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-test
+contract ProfleImageNfts is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    using strings for uint256;
+
+    Counters.Counter _tokenIds;
+
+    mapping(uint256 => string) _tokenURIs;
+
+    struct RenderToken {
+        uint256 id;
+        string uri;
+        string space;
+    }
+
+    constructor 
+}
